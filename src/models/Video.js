f5691808f2430145207e1c89d98756ea2bc28906
Bonghui(Nora) <74698994+Nora1476@users.noreply.github.com>
2024-01-10 16:22:53 +0900

    240110

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const vedioSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String, required: true, trim: true, minength: 10, maxLength: 80 },
+  description: { type: String, required: true, trim: true, minLength: 20 },
   createdAt: { type: Date, default: Date.now, required: true },
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, defualt: 0, required: true },
     rating: { type: Number, defualt: 0, required: true },
