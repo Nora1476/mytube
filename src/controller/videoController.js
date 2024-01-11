90389fb430ab5740810289832a5b17cb54292afd
Bonghui(Nora) <74698994+Nora1476@users.noreply.github.com>
2024-01-11 18:43:28 +0900

    240111

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -4,7 +4,8 @@ import Video from "../models/Video";
 //res.render(view, 넘겨줄 데이터) : view엔진으로 server.js에 등록된 view파일을 바로 렌더링 해줌
 //res.send() : html 형태로 바로 응답을 보냄
 export const home = async (req, res) => {
-  const videos = await Video.find({}) //mongoose와 연결된 모델파일 안에 Video를 통해 모든 비디오 데이터를 배열형태로 가져옴
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" }) //mongoose와 연결된 모델파일 안에 Video를 통해 모든 비디오 데이터를 배열형태로 가져옴
     .then((videos) => {
       res.render("home", { pageTitle: "Home", videos });
     })
@@ -70,4 +71,26 @@ export const postUpload = async (req, res) => {
     console.log(error);
     return res.render("upload", { pageTitle: "Upload Video", errorMessage: error._message });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        //몽고db가 가진 regular express객체로 title에서 키워드가 포함된 비디오를 찾아줌
+        $regex: new RegExp(`${keyword}`, "i"),
+        // $regex: new RegExp(`^${keyword}`, "i") -> keyword로 시작되는 것들을 검색.
+        // $regex: new RegExp(`${keyword}$`, "i") -> keyword로 끝나는 것들을 검색.
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { join, login } from "../controller/userController";
-import { home } from "../controller/videoController";
+import { home, search } from "../controller/videoController";
 //클린코드 위해 라우터에 반응하는 컨트롤러는 따로 폴더를 만들어 관리
 
 const globalRouter = express.Router();
@@ -8,5 +8,6 @@ const globalRouter = express.Router();
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
